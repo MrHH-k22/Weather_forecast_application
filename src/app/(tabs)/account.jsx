177df7fd9c5ad2ce6025e2eb22d1ee5b78a13606
Tran Trung Tin <trantrungtin254@gmail.com>
2025-05-01 16:11:44 +0700

    refactor: replace FooterTab and account components with new FormSignIn and FormSignUp components

--- src/app/(tabs)/account.jsx
+++ src/app/(tabs)/account.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Image } from "react-native";
 import React from "react";
+import FormSignIn from "../../components/formSignIn";
+import FormSignUp from "../../components/formSignUp";
 
 function account() {
   return (
-    <View>
-      <Text>account</Text>
+    <View className="relative flex-1">
+      <Image
+        blurRadius={70}
+        source={require("../../../assets/images/bg.png")}
+        className="absolute top-0 left-0 w-full h-full"
+      />
+      <FormSignIn />
     </View>
   );
 }
---
+++ src/components/formSignUp.jsx
@@ -0,0 +1,41 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+export default function FormSignUp() {
+  return (
+    <View className="flex-1 relative">
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-5xl font-bold text-yellow-400">
+          Nice to see you!
+        </Text>
+        <Text className="text-2xl font-bold text-white">
+          Create your account
+        </Text>
+        <Image
+          source={require("../../assets/icons/weather.png")}
+          className="w-52 h-52 mt-8"
+          style={{ tintColor: "#0D719B" }}
+        ></Image>
+        <View className="flex flex-col items-center mt-16">
+          <TextInput
+            placeholder="Username"
+            placeholderTextColor="white"
+            className="border border-gray-300 rounded-full p-2 w-80 mt-4"
+          />
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="white"
+            className="border border-gray-300 rounded-full p-2 w-80 mt-4"
+          />
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="white"
+            secureTextEntry={true}
+            className="border border-gray-300 rounded-full p-2 w-80 mt-4"
+          />
+          <TouchableOpacity className="bg-blue-500 rounded-full p-2 w-80 mt-4">
+            <Text className="text-white text-center">Create</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
